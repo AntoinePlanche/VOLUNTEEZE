b3d99d59be4a4a4c0d0d5772a11bbefd030b7203
aplanche <antoineplanche86@gmail.com>
2023-03-03 17:43:36 -0500

    réorganisation de l'application

--- FRONTEND/volunteeze/src/App.js
+++ FRONTEND/volunteeze/src/App.js
@@ -1,15 +1,15 @@
-import { Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Login from './pages/Login';
 import RegisterUser from './pages/RegisterUser';
 import RegisterAssociation from './pages/RegisterAssociation';
 import Redirection from './pages/App/Redirection';
-import TypesMissions from './pages/App/Information/TypesMissions';
-import Disponibilite from './pages/App/Information/Disponibilite';
+import TypesMissions from './pages/App/Benevoles/TypesMissions';
+import Disponibilite from './pages/App/Benevoles/Disponibilite';
 import RegisterType from './pages/RegisterType';
 import ResetPassword from './pages/ResetPassword';
-import AdresseAssociation from './pages/App/Information/AdresseAssociation';
-import AssociationPagePrincipal from './pages/AssociationPagePrincipal';
+import AdresseAssociation from './pages/App/Associations/AdresseAssociation'
+import PagePrincipale from './pages/App/Associations/PagePrincipale';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import 'bootstrap';
@@ -21,19 +21,21 @@ library.add(fab);
 function App() {
   return (
     <>
-      <Routes>
+      <Routes> 
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/login" element={<Login/>} />
         <Route exact path="/registertype" element={<RegisterType/>} />
         <Route exact path="/registeruser" element={<RegisterUser/>} />
         <Route exact path="/registerassociation" element={<RegisterAssociation/>} />
         <Route exact path='/resetpassword' element={<ResetPassword/>} />
-        <Route path="/information" element={<Redirection/>}>
-          <Route path="/information/types-missions" element={<TypesMissions />}/>
-          <Route path="/information/disponibilite" element={<Disponibilite />}/>
-          <Route path="/information/adresseassociation" element={<AdresseAssociation/>}/>
+        <Route path="/benevoles" element={<Redirection/>}>
+          <Route path="/benevoles/types-missions" element={<TypesMissions />}/>
+          <Route path="/benevoles/disponibilite" element={<Disponibilite />}/>
         </Route>
-        <Route path="/association/pageprincipal" element={<AssociationPagePrincipal/>}/>
+        <Route path="/associations" element={<Redirection/>}>
+          <Route path="/associations/adresseassociation" element={<AdresseAssociation/>}/>
+          <Route path="/association/pageprincipale" element={<PagePrincipale/>}/>
+        </Route>
       </Routes>
     </>
   );
