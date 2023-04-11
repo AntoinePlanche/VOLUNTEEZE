cdd90aef5d8fcad27e45b315ab1e63d303fe816e
aplanche <antoineplanche86@gmail.com>
2023-04-11 02:02:16 -0400

    test

--- API/database.py
+++ API/database.py
@@ -1,17 +1,15 @@
 from peewee import *
-from dotenv import load_dotenv, dotenv_values
-import os
 
-load_dotenv()
-
-user=os.environ.get("DATABASE_USERNAME")
-password=os.environ.get("DATABASE_PASSWORD")
-database=os.environ.get("DATABASE_NAME")
-unix_socket_path=os.environ.get("INSTANCE_UNIX_SOCKET")
+host="localhost"
+user="root"
+password="root"
+port=3306
+database="volunteeze"
 
 conn = MySQLDatabase(
-    database=database,
+    database = database,
+    host = host,
     user=user,
     password=password,
-    unix_socket=unix_socket_path
+    port=port
 )
