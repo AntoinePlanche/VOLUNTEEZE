2012d5e89dbef1eb08bb38a6448443c1a2342bbf
AntoinePlanche <AntoinePlanche@users.noreply.github.com>
2023-04-11 23:19:32 +0000

    Format code with black

--- API/routers/association.py
+++ API/routers/association.py
@@ -2,15 +2,19 @@ from typing import Any, List
 
 import peewee
 from fastapi import APIRouter, HTTPException, Response
-from models.association import create_association, get_association, list_associations, delete_association, update_association_adresse
+from models.association import (
+    create_association,
+    get_association,
+    list_associations,
+    delete_association,
+    update_association_adresse,
+)
 from pydantic import BaseModel
 from pydantic.utils import GetterDict
 
 
-router_association = APIRouter(
-    prefix="/association",
-    tags=["association"]
-)
+router_association = APIRouter(prefix="/association", tags=["association"])
+
 
 class PeeweeGetterDict(GetterDict):
     def get(self, key: Any, default: Any = None):
@@ -19,56 +23,76 @@ class PeeweeGetterDict(GetterDict):
             return list(res)
         return res
 
+
 class AssociationModel(BaseModel):
     id: int
     id_compte: int | None
-    nom:str | None
-    tel:str | None
-    description:str | None
-    adresse:str | None
-    longitude:float | None
-    latitude:float | None
-    logo:str | None
-    couverture:str | None
-
+    nom: str | None
+    tel: str | None
+    description: str | None
+    adresse: str | None
+    longitude: float | None
+    latitude: float | None
+    logo: str | None
+    couverture: str | None
 
     class Config:
         orm_mode = True
         getter_dict = PeeweeGetterDict
+
 
 class AssociationAdresseModel(BaseModel):
-    id_compte:int
-    adresse:str
-    longitude:float
-    latitude:float
+    id_compte: int
+    adresse: str
+    longitude: float
+    latitude: float
 
     class Config:
         orm_mode = True
         getter_dict = PeeweeGetterDict
+
 
 class AssociationRegisterModel(BaseModel):
-    id_compte:int
-    nom:str
-    tel:str
+    id_compte: int
+    nom: str
+    tel: str
 
     class Config:
         orm_mode = True
         getter_dict = PeeweeGetterDict
 
-@router_association.get("/", response_model=List[AssociationModel], summary="List of associations",
-                     description="Returns all associations!")
+
+@router_association.get(
+    "/",
+    response_model=List[AssociationModel],
+    summary="List of associations",
+    description="Returns all associations!",
+)
 def get_associations():
     return list_associations()
 
 
-@router_association.post("/", response_model=AssociationRegisterModel, summary="Create a new association")
-async def create(association : AssociationRegisterModel):
-    return await create_association(id_compte = association.id_compte, nom=association.nom, tel=association.tel)
+@router_association.post(
+    "/", response_model=AssociationRegisterModel, summary="Create a new association"
+)
+async def create(association: AssociationRegisterModel):
+    return await create_association(
+        id_compte=association.id_compte, nom=association.nom, tel=association.tel
+    )
 
 
-@router_association.post("/adresse", response_model=AssociationAdresseModel, summary="modifie l'adresse d'une association")
-async def update_adresse(association : AssociationAdresseModel):
-    return await update_association_adresse(id=association.id_compte, adresse=association.adresse, latitude=association.latitude, longitude=association.longitude)
+@router_association.post(
+    "/adresse",
+    response_model=AssociationAdresseModel,
+    summary="modifie l'adresse d'une association",
+)
+async def update_adresse(association: AssociationAdresseModel):
+    return await update_association_adresse(
+        id=association.id_compte,
+        adresse=association.adresse,
+        latitude=association.latitude,
+        longitude=association.longitude,
+    )
 
 
 @router_association.delete(
@@ -87,11 +111,15 @@ def remove_association(id: int):
     return Response(status_code=200)
 
 
-@router_association.get("/view/{id}", response_model=AssociationModel, summary="Returns a single association")
+@router_association.get(
+    "/view/{id}",
+    response_model=AssociationModel,
+    summary="Returns a single association",
+)
 async def view(id: int):
     """
-        To view all details related to a single association
-        - **id**: The integer id of the association you want to view details.
+    To view all details related to a single association
+    - **id**: The integer id of the association you want to view details.
     """
     association = get_association(id)
     if association is None:
